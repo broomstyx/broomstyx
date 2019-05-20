5dc745e7ea1945678a70dbd30b6f9d6b784802ff
Michael Sargado <michael.sargado@gmail.com>
2019-05-20 12:26:11 +0200

    [In Process] Adding poisson eqn tutorial.

---
+++ src/Numerics/Tutorial/Poisson_Fe_Tri3.cpp
@@ -0,0 +1,476 @@
+/*
+  Copyright (c) 2014 - 2019 University of Bergen
+  
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+#include "Poisson_Fe_Tri3.hpp"
+#include <cmath>
+#include <string>
+#include <vector>
+#include "../../Core/AnalysisModel.hpp"
+#include "../../Core/Cell.hpp"
+#include "../../Core/ObjectFactory.hpp"
+#include "../../Core/DomainManager.hpp"
+#include "../../Materials/Material.hpp"
+#include "../../Util/linearAlgebra.hpp"
+#include "../../BasisFunctions/Triangle_P1.hpp"
+
+using namespace broomstyx;
+
+registerBroomstyxObject(Numerics, Poisson_Fe_Tri3)
+
+// Numerics status
+NumericsStatus_Poisson_Fe_Tri3::NumericsStatus_Poisson_Fe_Tri3()
+    : _gradU(RealVector(2))
+{}
+
+NumericsStatus_Poisson_Fe_Tri3::~NumericsStatus_Poisson_Fe_Tri3()
+{}
+
+// Constructor
+Poisson_Fe_Tri3::Poisson_Fe_Tri3()
+{    
+    _dim = 2;
+    _dofPerNode = 1;
+    _nNodes = 3;
+    _nMaterials = 0;
+    _nStages = 1;
+    _nSubsystems = 1;
+    _name = "Poisson_Fe_Tri3";
+    
+    // Lone Gauss point coordinate is at natural coordinate (1/3, 1/3), weight = 0.5
+    _gpNatCoor = {1./3., 1./3.};
+    _wt = 0.5;
+    
+    // Pre-calculate shape functions and derivatives at Gauss point
+    _basisFunctionValues = _basisFunction.giveBasisFunctionsAt(_gpNatCoor);
+    std::vector<RealVector> dpsiNat = _basisFunction.giveBasisFunctionDerivativesAt(_gpNatCoor);
+    _basisFunctionDerivatives.init(2,3);
+    for ( int i = 0; i < 3; i++)
+    {
+        _basisFunctionDerivatives(0,i) = dpsiNat[0](i);
+        _basisFunctionDerivatives(1,i) = dpsiNat[1](i);
+    }
+}
+
+// Destructor
+Poisson_Fe_Tri3::~Poisson_Fe_Tri3()
+{}
+
+// Private methods
+// ----------------------------------------------------------------------------
+void Poisson_Fe_Tri3::deleteNumericsAt(Cell* targetCell)
+{
+    delete cns;
+}
+
+void Poisson_Fe_Tri3::finalizeDataAt( Cell* targetCell )
+{
+    // Retrieve numerics status at cell
+    auto cns = this->getNumericsStatusAt(targetCell);
+    
+    // Retrieve nodal DOFs local to element
+    std::vector<Dof*> dof = this->giveNodalDofsAt(targetCell);
+    
+    // Retrieve DOF values
+    RealVector uVec = this->giveLocalValuesAt(dof, converged_value);
+    
+    // Construct gradient
+    RealMatrix bmat = this->giveBmatAt(targetCell);
+    cns->_gradU = bmat*uVec;
+}
+// ----------------------------------------------------------------------------
+double Poisson_Fe_Tri3::giveCellFieldValueAt( Cell* targetCell, int fieldNum )
+{
+    RealVector val;
+    std::string fieldTag;
+    
+    try
+    {
+        fieldTag = _cellFieldOutput.at(fieldNum);
+    }
+    catch (std::exception& e)
+    {
+        fieldTag = "unassigned";
+    }
+    
+    std::tie(val, std::ignore) = this->giveFieldOutputAt(targetCell, fieldTag);
+    
+    return val(0);
+}
+// ----------------------------------------------------------------------------
+RealVector Poisson_Fe_Tri3::giveCellNodeFieldValuesAt( Cell* targetCell, int fieldNum )
+{
+    double val = this->giveCellFieldValueAt(targetCell, fieldNum);
+    
+    RealVector cellNodeValues(3);
+    cellNodeValues(0) = val;
+    cellNodeValues(1) = val;
+    cellNodeValues(2) = val;
+    
+    return cellNodeValues;
+}
+// ----------------------------------------------------------------------------
+std::vector<RealVector> Poisson_Fe_Tri3::giveEvaluationPointsFor( Cell *targetCell)
+{
+    std::vector<Node*> node = analysisModel().domainManager().giveNodesOf(targetCell);
+    
+    std::vector<RealVector> coor(1, RealVector());
+    coor[0].init(3);
+    
+    for ( int i = 0; i < 3; i++)
+    {
+        RealVector nodeCoor = analysisModel().domainManager().giveCoordinatesOf(node[i]);
+        coor[0](0) += nodeCoor(0)/3.0;
+        coor[0](1) += nodeCoor(1)/3.0;
+        coor[0](2) += nodeCoor(2)/3.0;
+    }
+    
+    return coor;
+}
+// ----------------------------------------------------------------------------
+std::tuple< RealVector, RealVector >
+Poisson_Fe_Tri3::giveFieldOutputAt( Cell* targetCell, const std::string& fieldTag )
+{
+    RealVector fieldVal(1), weight(1);
+    
+    auto cns = this->getNumericsStatusAt(targetCell);
+    weight(0) = _wt*cns->_Jdet;
+    
+    std::vector<Material*> material = this->giveMaterialSetFor(targetCell);
+    
+    if ( fieldTag == "unassigned" )
+        fieldVal(0) = 0.;
+    else if ( fieldTag == "u_x" )
+        fieldVal(0) = cns->_gradU(0);
+    else if ( fieldTag == "u_y" )
+        fieldVal(0) = cns->_gradU(1);
+    else
+        throw std::runtime_error("Invalid tag '" + fieldTag + "' supplied in field output request made to numerics '" + _name + "'!");
+    
+    return std::make_tuple(std::move(fieldVal), std::move(weight));
+}
+// ----------------------------------------------------------------------------
+std::tuple< std::vector<Dof*>
+          , std::vector<Dof*>
+          , RealVector >
+Poisson_Fe_Tri3::giveStaticCoefficientMatrixAt( Cell*           targetCell
+                                              , int             stage
+                                              , int             subsys
+                                              , const TimeData& time )
+{
+    std::vector<Dof*> rowDof, colDof;
+    RealVector coefVal;
+    
+    if ( stage == _stage[0] && (subsys == _subsystem[0] || subsys == UNASSIGNED) )
+    {
+        // Retrieve nodal DOFs local to element
+        std::vector<Dof*> dof = this->giveNodalDofsAt(targetCell);
+        
+        // Calculate stiffness matrix
+        RealMatrix bmat = this->giveBmatAt(targetCell);
+        RealMatrix kmat = _wt*cns->_Jdet*trp(bmat)*bmat;
+        
+        rowDof.assign(9, nullptr);
+        colDof.assign(9, nullptr);
+        coefVal.init(9);
+        
+        int counter = 0;
+        for ( int i = 0; i < 3; i++)
+            for ( int j = 0; j < 3; j++)
+            {
+                rowDof[counter] = dof[i];
+                colDof[counter] = dof[j];
+                coefVal(counter) = kmat(i,j);
+                ++counter;
+            }
+    }
+    
+    return std::make_tuple(std::move(rowDof), std::move(colDof), std::move(coefVal));
+}
+// ----------------------------------------------------------------------------
+std::tuple< std::vector<Dof*>
+          , RealVector >
+Poisson_Fe_Tri3::giveStaticLeftHandSideAt(Cell*            targetCell
+                                         , int             stage
+                                         , int             subsys
+                                         , const TimeData& time )
+{
+    std::vector<Dof*> rowDof;
+    RealVector lhs;
+    
+    if ( stage == _stage[0] && (subsys == _subsystem[0] || subsys == UNASSIGNED) )
+    {
+        // Retrieve numerics status
+        auto cns = this->getNumericsStatusAt(targetCell);
+        
+        // Element area and local displacements
+        rowDof = this->giveNodalDofsAt(targetCell);
+        RealVector u = this->giveLocalDisplacementsAt(rowDof, current_value);
+            
+        // Compute local gradients
+        RealMatrix bmat = giveBmatAt(targetCell);
+        cns->_gradU = bmat*u;
+        
+        // Calculate lhs
+        lhs = _wt*cns->_Jdet*trp(bmat)*cns->_gradU;
+    }
+    
+    return std::make_tuple(std::move(rowDof), std::move(lhs));
+}
+// ----------------------------------------------------------------------------
+std::tuple< std::vector<Dof*>
+          , RealVector >
+Poisson_Fe_Tri3::giveStaticRightHandSideAt( Cell*                    targetCell
+                                          , int                      stage
+                                          , int                      subsys
+                                          , const BoundaryCondition& bndCond
+                                          , const TimeData&          time )
+{   
+    std::vector<Dof*> rowDof;
+    RealVector rhs;
+    
+    if ( stage == _stage[0] && (subsys == _subsystem[0] || subsys == UNASSIGNED) )
+    {
+        if ( bndCond.conditionType() == "Traction" )
+        {
+            // Retrieve nodes of boundary element
+            std::vector<Node*> node = analysisModel().domainManager().giveNodesOf(targetCell);
+            
+            // ----------------------------------------------------------------
+            // Note: Boundary element must be a 2-node line
+            // ----------------------------------------------------------------
+            
+            if ( (int)node.size() != 2 )
+                throw std::runtime_error("Error: Traction boundary condition for '" + _name + "' requires 2-node boundary elements!");
+            
+            RealVector coor0, coor1, dx;
+            coor0 = analysisModel().domainManager().giveCoordinatesOf(node[0]);
+            coor1 = analysisModel().domainManager().giveCoordinatesOf(node[1]);
+            dx = coor1 - coor0;
+            double length = std::sqrt(dx.dot(dx));
+
+            // Determine proper value of boundary condition
+            RealVector midpt = 0.5*(coor0 + coor1);
+            double bcVal = bndCond.valueAt(midpt, time);
+
+            // Construct local RHS vector (only for relevant DOFs)
+            rhs.init(2);
+            rhs(0) = 0.5*length*bcVal;
+            rhs(1) = 0.5*length*bcVal;
+
+            // Construct global address vector
+            rowDof.assign(2, nullptr);
+
+            // Note that bndCond.targetDof() assumes 1-based notation but
+            // first element of nodalDof is stored at index 0!
+            // So you need to make adjustments
+            int dofNum = _nodalDof[bndCond.targetDof() - 1];
+
+            rowDof[0] = analysisModel().domainManager().giveNodalDof(dofNum, node[0]);
+            rowDof[1] = analysisModel().domainManager().giveNodalDof(dofNum, node[1]);
+        }
+        else if ( bndCond.conditionType() == "ConcentratedForce" ) // Boundary conditions for 1-node point
+        {
+            // Retrieve nodes of boundary element
+            std::vector<Node*> node = analysisModel().domainManager().giveNodesOf(targetCell);
+            
+            // ----------------------------------------------------------------
+            // Note: Boundary element must be a 1-node point
+            // ----------------------------------------------------------------
+            
+            if ( (int)node.size() != 1 )
+                throw std::runtime_error("Error: Concentrated force boundary condition for '" + _name + "' requires 1-node boundary elements!");
+            
+            // Determine proper value of boundary condition
+            RealVector coor = analysisModel().domainManager().giveCoordinatesOf(node[0]);
+            double bcVal = bndCond.valueAt(coor, time);
+
+            // Construct local RHS vector (only for relevant DOFs)
+            rhs.init(1);
+            rhs(0) = bcVal;
+
+            // Construct global address vector
+            rowDof.assign(1, nullptr);
+
+            // Note that bndCond.targetDof() assumes 1-based notation but
+            // first element of nodalDof is stored at index 0!
+            // So you need to make adjustments
+            int dofNum = _nodalDof[bndCond.targetDof() - 1];
+
+            rowDof[0] = analysisModel().domainManager().giveNodalDof(dofNum, node[0]);
+        }
+    }
+    
+    return std::make_tuple(std::move(rowDof), std::move(rhs));
+}
+// ----------------------------------------------------------------------------
+std::tuple< std::vector<Dof*>
+          , RealVector >
+Poisson_Fe_Tri3::giveStaticRightHandSideAt( Cell*                 targetCell
+                                          , int                   stage
+                                          , int                   subsys
+                                          , const FieldCondition& fldCond
+                                          , const TimeData&       time )
+{
+    std::vector<Dof*> rowDof;
+    RealVector rhs;
+    
+    if ( stage == _stage[0] && (subsys == _subsystem[0] || subsys == UNASSIGNED) )
+    {
+        // Evaluate body forces arising from field conditions
+        std::string cndType = fldCond.conditionType();
+        if ( cndType == "SourceTerm" )
+        {
+            //auto cns = this->getNumericsStatusAt(targetCell);
+            
+            // Get element density
+            //std::vector<Material*> material = this->giveMaterialSetFor(targetCell);
+            //double rho = material[0]->giveMaterialVariable("Density", cns->_materialStatus[0]);
+
+            // Retrieve nodal DOFs for element
+            rowDof = this->giveNodalDofsAt(targetCell);
+            
+            // Note that field condition is assumed to be piecewise linear over each cell
+            std::vector<RealVector> coor = this->giveEvaluationPointsFor(targetCell);
+            double val = fldCond.valueAt(coor[0], time)*_wt*cns->_Jdet/3.0;
+            
+            rhs.init(3);
+            rhs(0) = val;
+            rhs(1) = val;
+            rhs(2) = val;
+        }        
+    }
+    
+    return std::make_tuple(std::move(rowDof), std::move(rhs));
+}
+// ----------------------------------------------------------------------------
+void Poisson_Fe_Tri3::imposeConstraintAt( Cell*                    targetCell
+                                        , int                      stage
+                                        , const BoundaryCondition& bndCond
+                                        , const TimeData&          time )
+{                    
+    // Only for essential BCs on nodal DOFs
+    if ( bndCond.conditionType() == "NodalConstraint" )
+    {
+        // Retrieve nodes of boundary element
+        std::vector<Node*> node = analysisModel().domainManager().giveNodesOf(targetCell);
+        int targetDofNum = _nodalDof[bndCond.targetDof() - 1];
+        
+        for ( int j = 0; j < (int)node.size(); j++)
+        {
+            Dof* targetDof = analysisModel().domainManager().giveNodalDof(targetDofNum, node[j]);
+            RealVector coor = analysisModel().domainManager().giveCoordinatesOf(node[j]);
+            double bcVal = bndCond.valueAt(coor, time);
+            analysisModel().dofManager().setConstraintValueAt(targetDof, bcVal);
+        }
+    }
+}
+// ----------------------------------------------------------------------------
+void Poisson_Fe_Tri3::initializeNumericsAt( Cell* targetCell )
+{
+    targetCell->numericsStatus = new NumericsStatus_PlaneStrain_Fe_Tri3();
+    auto cns = this->getNumericsStatusAt(targetCell);
+    
+    // Pre-calculate det(J) and inv(J);
+    RealMatrix Jmat = this->giveJacobianMatrixAt(targetCell, _gpNatCoor);
+    cns->_Jdet = Jmat(0,0)*Jmat(1,1) - Jmat(1,0)*Jmat(0,1);
+    
+    // Sanity check
+    if ( cns->_Jdet <= 0 )
+        throw std::runtime_error("Calculation of negative area detected!\nSource: " + _name);
+    
+    cns->_JmatInv = inv(Jmat);
+}
+// ----------------------------------------------------------------------------
+void Poisson_Fe_Tri3::setDofStagesAt( Cell* targetCell )
+{
+    // Element uses only one stage i.e. stage[0], and all nodal degrees of 
+    // freedom get assigned to this stage
+    std::vector<Node*> node = analysisModel().domainManager().giveNodesOf(targetCell);
+    
+    for ( int i = 0; i < (int)node.size(); i++)
+    {
+        Dof* dof = analysisModel().domainManager().giveNodalDof(_nodalDof[0],node[i]);
+        analysisModel().dofManager().setStageFor(dof, _stage[0]);
+    }
+}
+
+// Private methods
+// ---------------------------------------------------------------------------
+NumericsStatus_Poisson_Fe_Tri3*
+Poisson_Fe_Tri3::getNumericsStatusAt( Cell* targetCell )
+{
+    auto cns = dynamic_cast<NumericsStatus_Poisson_Fe_Tri3*>(targetCell->numericsStatus);
+    if ( !cns )
+        throw std::runtime_error("Error: Unable to retrieve numerics status at cell!\nSource: " + _name);
+    
+    return cns;
+}
+// ----------------------------------------------------------------------------
+RealMatrix Poisson_Fe_Tri3::giveBmatAt( Cell* targetCell )
+{
+    auto cns = this->getNumericsStatusAt(targetCell);
+    return cns->_JmatInv*_basisFunctionDerivatives;
+}
+// ----------------------------------------------------------------------------
+RealMatrix Poisson_Fe_Tri3::giveJacobianMatrixAt( Cell* targetCell, const RealVector& natCoor )
+{
+    std::vector<Node*> node = analysisModel().domainManager().giveNodesOf(targetCell);
+#ifndef NDEBUG
+    if ( (int)node.size() != 3 )
+        throw std::runtime_error("\nError: Basis function requires 3 nodes be specified for calculation of Jacobian matrix!\nSource: " + _name);
+#endif
+    
+    RealMatrix coorMat(3,2);
+    
+#pragma GCC ivdep
+    for ( int i = 0; i < 3; i++ )
+    {
+        RealVector nodeCoor = analysisModel().domainManager().giveCoordinatesOf(node[i]);
+        coorMat(i,0) = nodeCoor(0);
+        coorMat(i,1) = nodeCoor(1);
+    }
+    
+   return _basisFunctionDerivatives*coorMat;
+}
+// ----------------------------------------------------------------------------
+RealVector Poisson_Fe_Tri3::giveLocalValuesAt( std::vector<Dof*>& dof, ValueType valType)
+{    
+    RealVector u(3);
+    u(0) = analysisModel().dofManager().giveValueOfPrimaryVariableAt(dof[0], valType);
+    u(1) = analysisModel().dofManager().giveValueOfPrimaryVariableAt(dof[1], valType);
+    u(2) = analysisModel().dofManager().giveValueOfPrimaryVariableAt(dof[2], valType);
+
+    return u;
+}
+// ----------------------------------------------------------------------------
+std::vector<Dof*> Poisson_Fe_Tri3::giveNodalDofsAt( Cell* targetCell )
+{
+    std::vector<Node*> node = analysisModel().domainManager().giveNodesOf(targetCell);
+    std::vector<Dof*> dof(3, nullptr);
+
+    dof[0] = analysisModel().domainManager().giveNodalDof(_nodalDof[0], node[0]);
+    dof[1] = analysisModel().domainManager().giveNodalDof(_nodalDof[0], node[1]);
+    dof[2] = analysisModel().domainManager().giveNodalDof(_nodalDof[0], node[2]);
+
+    return dof;
+}
---
+++ src/Numerics/Tutorial/Poisson_Fe_Tri3.hpp
@@ -0,0 +1,141 @@
+/*
+  Copyright (c) 2014 - 2019 University of Bergen
+  
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+/* --------------------------------------------------------------------------
+ * Input file declaration:
+ * 
+ * ELEMENT_TYPES
+ *    ...
+ *    <tag> Poisson_Fe_Tri3
+ *            NodalDof <dof1>
+ *            Stage <stg>
+ *            CellFieldOutput <n2>
+ *                   ...             ...
+ *                   ...             ...
+ * 
+ * Field data tags:
+ * 
+ *  u_x : gradient along x
+ *  u_y : gradient along y
+ * 
+ ****************************************************************************/
+
+#ifndef POISSON_FE_TRI3_HPP
+#define	POISSON_FE_TRI3_HPP
+
+#include "../Numerics.hpp"
+#include "../../Core/DofManager.hpp"
+#include "../../Core/NumericsManager.hpp"
+#include "../../BasisFunctions/Triangle_P1.hpp"
+
+namespace broomstyx
+{
+    class MaterialStatus;
+        
+    class NumericsStatus_Poisson_Fe_Tri3 final : public NumericsStatus
+    {
+        friend class Poisson_Fe_Tri3;
+        
+    public:
+        NumericsStatus_Poisson_Fe_Tri3();
+        virtual ~NumericsStatus_Poisson_Fe_Tri3();
+        
+    private:
+        RealVector _gradU;
+        RealMatrix _JmatInv;
+        double     _Jdet;
+    };
+    
+    class Poisson_Fe_Tri3 final : public Numerics 
+    {
+    public:
+        Poisson_Fe_Tri3();
+        virtual ~Poisson_Fe_Tri3();
+
+        void   deleteNumericsAt( Cell* targetCell ) override;
+        void   finalizeDataAt( Cell* targetCell ) override;
+        double giveCellFieldValueAt( Cell* targetCell, int fieldNum ) override;
+        
+        RealVector 
+            giveCellNodeFieldValuesAt( Cell* targetCell, int fieldNum ) override;
+        
+        std::vector<RealVector> 
+            giveEvaluationPointsFor( Cell* targetCell ) override;
+        
+        std::tuple< RealVector,RealVector > 
+            giveFieldOutputAt( Cell* targetCell, const std::string& fieldTag ) override;
+        
+        std::tuple< std::vector<Dof*>
+                  , std::vector<Dof*>
+                  , RealVector >
+            giveStaticCoefficientMatrixAt( Cell*           targetCell
+                                         , int             stage
+                                         , int             subsys
+                                         , const TimeData& time ) override;
+        
+        std::tuple< std::vector<Dof*>, RealVector >
+            giveStaticLeftHandSideAt( Cell*           targetCell
+                                    , int             stage
+                                    , int             subsys
+                                    , const TimeData& time ) override;
+        
+        std::tuple< std::vector<Dof*>, RealVector >
+            giveStaticRightHandSideAt( Cell*                    targetCell
+                                     , int                      stage
+                                     , int                      subsys
+                                     , const BoundaryCondition& bndCond
+                                     , const TimeData&          time ) override;
+
+        std::tuple< std::vector<Dof*>, RealVector >
+            giveStaticRightHandSideAt( Cell*                 targetCell
+                                     , int                   stage
+                                     , int                   subsys
+                                     , const FieldCondition& fldCond
+                                     , const TimeData&       time ) override;
+        
+        void imposeConstraintAt( Cell*                    targetCell
+                               , int                      stage
+                               , const BoundaryCondition& bndCond
+                               , const TimeData&          time ) override;
+        
+        void initializeNumericsAt( Cell* targetCell ) override;
+        void setDofStagesAt( Cell* targetCell ) override;
+
+    private:
+        Triangle_P1 _basisFunction;
+        RealVector  _basisFunctionValues;
+        RealMatrix  _basisFunctionDerivatives;
+        
+        RealVector  _gpNatCoor;
+        double      _wt;
+        
+        NumericsStatus_Poisson_Fe_Tri3*
+                   getNumericsStatusAt( Cell* targetCell );
+        RealMatrix giveBmatAt( Cell* targetCell );
+        RealMatrix giveJacobianMatrixAt( Cell* targetCell, const RealVector& natCoor );
+        RealVector giveLocalValuesAt( std::vector<Dof*>& dof, ValueType valType );
+        std::vector<Dof*> giveNodalDofsAt( Cell* targetCell );
+    };
+}
+
+#endif	/* POISSON_FE_TRI3_HPP */
