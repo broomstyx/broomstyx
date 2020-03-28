8bd154622e583f1601cd2709ce9ecf76f556908a
Michael Sargado <michael.sargado@gmail.com>
2020-03-28 22:37:51 +0100

    Transfer of Torsion warping function to User folder

---
+++ src/User/Torsion/StVenantTorsion_ErrNorm_WarpFcn.hpp
@@ -0,0 +1,54 @@
+/*
+  Copyright (c) 2014 - 2019 University of Bergen
+  
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+#ifndef STVENANTTORSION_ERRNORM_WARPFCN_HPP
+#define STVENANTTORSION_ERRNORM_WARPFCN_HPP
+
+#include "OutputQuantities/OutputQuantity.hpp"
+
+namespace broomstyx
+{
+    class UserFunction;
+    
+    class StVenantTorsion_ErrNorm_WarpFcn final : public OutputQuantity
+    {
+    public:
+        StVenantTorsion_ErrNorm_WarpFcn();
+        virtual ~StVenantTorsion_ErrNorm_WarpFcn();
+        
+        double computeOutput() override;
+        void   initialize() override;
+        void   readDataFrom( FILE* fp ) override;
+    
+    private:
+        int         _numericsTag;
+        std::string _domainTag;
+        int         _dofNum;
+        std::string _fcnName;
+        
+        UserFunction* _analyticSoln;
+    };
+}
+
+#endif /* STVENANTTORSION_ERRNORM_WARPFCN_HPP */
+
