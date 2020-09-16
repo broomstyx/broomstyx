ba8c2ed011bed1e8d9d7c8f50c74c717c6200f77
jmsargado <michael.sargado@gmail.com>
2020-09-16 11:12:38 +0200

    Layered surfing problem

---
+++ src/User/LayeredSurfingProblem/LayeredSurfingBC_dispX.hpp
@@ -0,0 +1,43 @@
+/*
+  Copyright (c) 2014 - 2019 University of Bergen
+  
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+#ifndef LAYEREDSURFINGBC_DISPX_HPP
+#define LAYEREDSURFINGBC_DISPX_HPP
+
+#include "User/UserFunction.hpp"
+
+namespace broomstyx
+{
+    class LayeredSurfingBC_dispX : public UserFunction
+    {
+    public:
+        LayeredSurfingBC_dispX();
+        virtual ~LayeredSurfingBC_dispX();
+        
+        double at( const RealVector& coor, const TimeData& time ) override;
+
+    private:
+    };
+}
+
+#endif /* LAYEREDSURFINGBC_DISPX_HPP */
---
+++ src/User/LayeredSurfingProblem/LayeredSurfingBC_dispY.cpp
@@ -0,0 +1,63 @@
+/*
+  Copyright (c) 2014 - 2019 University of Bergen
+  
+  This file is part of the BROOMStyx project.
+
+  BROOMStyx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  BROOMStyx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with BROOMStyx.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the AUTHORS file
+  for the list of copyright holders.
+*/
+
+#include "LayeredSurfingBC_dispY.hpp"
+
+#include <cmath>
+#include "Core/ObjectFactory.hpp"
+
+using namespace broomstyx;
+        
+registerBroomstyxObject(UserFunction, LayeredSurfingBC_dispY)
+
+LayeredSurfingBC_dispY::LayeredSurfingBC_dispY() {}
+
+LayeredSurfingBC_dispY::~LayeredSurfingBC_dispY() {}
+
+double LayeredSurfingBC_dispY::at( const RealVector& coor, const TimeData& time )
+{
+    double PI = 4.*std::atan(1.);
+
+    // Hard-coded parameters
+    double Gc = 1.;
+    double E  = 1.;
+    double nu = 0.3;
+
+    double x = coor(0) - time.target;
+    double y = coor(1);
+
+    // convert to polar coordinates
+    double r = std::sqrt(x*x + y*y);
+    double theta = std::acos(x/r);
+    if ( y < 0 )
+        theta = -theta;
+
+    // Stress intensity factor
+    double KI = std::sqrt(E*Gc);
+
+    // Kolosov constant for plane strain
+    double kappa = 3. - 4.*nu;
+
+    // Calculate y-displacement
+    return KI * (1. + nu)/E * std::sqrt(r/(2.*PI)) * (kappa - x/r) * std::sin(theta/2.);
+}
